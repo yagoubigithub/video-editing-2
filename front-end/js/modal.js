bdd6191968d8b178cf67c5da297766d46c1e467f
unknown <yagoubi.aek.2@gmail.com>
2023-10-18 01:46:37 +0100

    first step modal

---
+++ front-end/js/modal.js
@@ -0,0 +1,29 @@
+function myModel(modalId, btnId, closeId) {
+  const model = document.getElementById(modalId);
+
+  // Get the button that opens the modal
+  const btn = document.getElementById(btnId);
+
+  // Get the <span> element that closes the modal
+  const span = document.getElementById(closeId);
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    model.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    model.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == model) {
+      model.style.display = "none";
+    }
+  };
+}
+
+
+myModel("videos-model" , "btn-open-videos" , "close-videos-modal")
